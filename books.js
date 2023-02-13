d5d74e888890710659feebd41ae8d9e7eb881424
Arthur GLANDIER <arthur.glandier@gmail.com>
2023-02-13 12:05:03 +0100

    initializing a first list of books
    Initialized test book list

--- books.js
+++ books.js
@@ -1,6 +1,14 @@
 const num_books = 100;
 
-const Titles = ['Misery', 'It*', 'It**', 'Night Shift', 'The Shining', 'Carrie', 'Et on tuera tous les affreux', 'En avant la zizique',
+const titles = ['Misery', 'It*', 'It**', 'Night Shift', 'The Shining', 'Carrie', 'Et on tuera tous les affreux', 'En avant la zizique',
     "L'écume des jours", 'Le Hobbit', 'Les âmes croisées', "L'appel de Ctulhu", 'Les montagnes hallucinées']
 
-const Authors = ['Stephen King', "Boris Vian", "J.R.R. Tolkien", "Pierre Bottero", "H.P. Lovecraft"]
+const authors = ['Stephen King', 'Stephen King', 'Stephen King', 'Stephen King', 'Stephen King', 'Stephen King', "Boris Vian",
+"Boris Vian", "Boris Vian", "J.R.R. Tolkien", "Pierre Bottero", "H.P. Lovecraft", "H.P. Lovecraft"]
+
+const getTitle = (index) => `${titles[index]}`
+const getAuthor = (index) => `${authors[index]}`
+
+
+// create a book
+const createBook = (index) => ({title: getTitle(index), author: getAuthor(index)})
