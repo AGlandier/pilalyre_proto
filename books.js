13ffd22152542114f18e87589b14902477ba2f6f
Arthur GLANDIER <aglandier@consultants-solutec.fr>
2023-02-13 11:18:27 +0100

    first commit

---
+++ books.js
@@ -0,0 +1,6 @@
+const num_books = 100;
+
+const Titles = ['Misery', 'It*', 'It**', 'Night Shift', 'The Shining', 'Carrie', 'Et on tuera tous les affreux', 'En avant la zizique',
+    "L'écume des jours", 'Le Hobbit', 'Les âmes croisées', "L'appel de Ctulhu", 'Les montagnes hallucinées']
+
+const Authors = ['Stephen King', "Boris Vian", "J.R.R. Tolkien", "Pierre Bottero", "H.P. Lovecraft"]
